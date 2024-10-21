4cdda18925e0e6a1115e9f55ec0daad58c035c17
Brian-Samson01 <brian.s.mwangi@gmail.com>
2024-10-21 22:32:11 +0300

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+const javaclass= Document.getElementById('colorButton')
+javaclass.addEventListener('click', ()=>{
+    javaclass.style.backgroundColor= javaclass.style.backgroundColor==='blue' ? '#4caf59':'blue'
+});
